99fa1702d266e3fd289b6ea3f61c60a3d986ca84
odunayo <okebunmiodunayo@gmail.com>
2020-02-11 23:55:22 +0100

    broken homepage into smaller components style menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.compoenent'
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
