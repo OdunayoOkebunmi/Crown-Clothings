f95f9f4fe9ab502196b5486bd7ccbf2bc97881ad
odunayo <okebunmiodunayo@gmail.com>
2020-03-04 08:29:40 +0100

    added express server to handle stripe API, separated BE and FE

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 0.275115;
+  const priceForStripe = ((price * 0.0027).toFixed(2)) * 100;
   const publishableKey = 'pk_test_njE796lB7WwtdaAEZhgDizZ700kknmdltj';
-  const onToken = (token) => {
-    console.log(token)
-    alert('Payment Successful')
-  }
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
   return (
     <StripeCheckout
       label='Pay Now'
