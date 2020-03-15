2966feac4e7491eab52da78f05e7bd9e4ba43096
odunayo <okebunmiodunayo@gmail.com>
2020-03-15 01:42:46 +0100

    fix service worker issue

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -32,7 +33,7 @@ app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
 });
-app.get('./registerServiceWorker.js', (req, res) => {
+app.get('/registerServiceWorker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'registerServiceWorker.js'))
 })
 app.post('/payment', (req, res) => {
